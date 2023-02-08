5a03d1242c6930b644a7e3d41749e32c01cde7d4
sfincks <sfincks666@gmail.com>
2023-02-08 21:27:57 +0500

    zadacha_03

--- Zadacha_03/Program.cs
+++ Zadacha_03/Program.cs
@@ -2,3 +2,20 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19] 
 // 6, 1, 33 -> [6, 1, 33]
 
+void getArray(int[] array, int size, int start, int end)
+{
+    Random rng = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rng.Next(start, end + 1); //+1 для того чтобы число end было включено в диапозон рнг чисел
+    }
+}
+
+void printArray(int[] array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+int[] massiv = new int[8];
+getArray(massiv, 8, 77, 777);
+printArray(massiv);
