a5e98121eaabd4b7f869a1013afc36aee7f1055a
sfincks <sfincks666@gmail.com>
2023-02-08 19:29:49 +0500

    zadacha_01

--- Zadacha_01/Program.cs
+++ Zadacha_01/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+//  и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int GetNumber (string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int firstNumber = GetNumber("Введите первое число");
+int secondNumber = GetNumber("Введите второе число");
+
+int PowerOfNumber (int number,int power)
+{
+    int result = 1;
+    for (int i = 0; i < power; i++)
+    {
+        result = result*number;
+    }
+    return result;
+}
+
+// System.Console.WriteLine(Math.Pow(firstNumber,secondNumber));
+System.Console.WriteLine(PowerOfNumber(firstNumber,secondNumber));
+
